d0c15bdb22aee141fe8dfcf152a38e936e19a601
NatarajVeerapur <natarajv.mca22@rvce.edu.in>
2023-06-28 22:53:19 +0530

    provided problem url

--- electronicShop.py
+++ electronicShop.py
@@ -1,5 +1,8 @@
-
-
+"""A person wants to determine the most expensive computer keyboard and 
+USB drive that can be purchased with a give budget. Given price lists for keyboards and USB drives 
+and a budget, find the cost to buy them. If it is not possible to buy both items, return ."""
+#url
+#https://www.hackerrank.com/challenges/electronics-shop/problem
 def getMoneySpent(keyboards, drives, b):
     x=0
     y=[-1]
